updateing/kernel-google-modules-display

commit 64d97619a2b0064f64c7bc9178934213b9a51be3

GKI: drm: samsung: introduce EXYNOS_DSI_MSG_QUEUE flag
Commit 1026fb2f81df1 ("ANDROID: drivers: gpu: drm: add support to batch
commands)" was reverted in ACK breaking the ability to batch commands
sucessfully. Since MIPI_DSI_MSG_LASTCOMMAND flag was no longer set on
mipi_dsi_device_transfer(), the commands would be kept queued until the
number of pending packets exceeded its threshold. This resulted in a
visible lag on the display (specially when UART was enabled).

This patch introduces the EXYNOS_DSI_MSG_QUEUE flag as an inverse logic
replacement of the reverted MIPI_DSI_MSG_LASTCOMMAND flag. Commands not
marked with EXYNOS_DSI_MSG_QUEUE flag should now be considered the last
command of the batch and be sent immediately.

Bug: 201090883
Test: Batching works. Display lag fixed even with UART enabled.
Suggested-by: Adrian Salido <salidoa@google.com>
Signed-off-by: Carlos Llamas <cmllamas@google.com>
Change-Id: I2662d96e66a21c38567e46ea36bab54f03355048

samsung/exynos_drm_connector.h
@@ -29,8 +29,8 @@ struct exynos_drm_connector;
 /** Private DSI msg flags **/
 
 /* Stack all commands until lastcommand bit and trigger all in one go */
-#ifndef MIPI_DSI_MSG_LASTCOMMAND
-#define MIPI_DSI_MSG_LASTCOMMAND BIT(3)
+#ifndef EXYNOS_DSI_MSG_QUEUE
+#define EXYNOS_DSI_MSG_QUEUE BIT(13)
 #endif
 
 /* packetgo feature to batch msgs can wait for vblank, use this flag to ignore */

samsung/exynos_drm_dsim.c
@@ -1786,7 +1786,7 @@ static void need_wait_vblank(struct dsim_device *dsim)
 }
 
 #define PL_FIFO_THRESHOLD	mult_frac(MAX_PL_FIFO, 75, 100) /* 75% */
-#define IS_LAST(flags)		(((flags) & MIPI_DSI_MSG_LASTCOMMAND) != 0)
+#define IS_LAST(flags)		(((flags) & EXYNOS_DSI_MSG_QUEUE) == 0)
 static int
 dsim_write_data(struct dsim_device *dsim, const struct mipi_dsi_msg *msg)
 {
@@ -1814,7 +1814,7 @@ dsim_write_data(struct dsim_device *dsim, const struct mipi_dsi_msg *msg)
 		((dsim->total_pend_pl + msg->tx_len) > PL_FIFO_THRESHOLD))) {
 		dsim_warn(dsim, "warning. changed last command. pend pl/pl(%u,%u)\n",
 				dsim->total_pend_ph, dsim->total_pend_pl);
-		flags |= MIPI_DSI_MSG_LASTCOMMAND;
+		flags &= ~EXYNOS_DSI_MSG_QUEUE;
 	}
 
 	dsim_debug(dsim, "%s last command\n", IS_LAST(flags) ? "" : "Not");

samsung/panel/panel-samsung-drv.c
@@ -684,8 +684,8 @@ void exynos_panel_send_cmd_set_flags(struct exynos_panel *ctx,
 		dsi_flags |= EXYNOS_DSI_MSG_IGNORE_VBLANK;
 
 	/* if not batched or queued, all commands should be sent out immediately */
-	if (!(flags & async_mask))
-		dsi_flags |= MIPI_DSI_MSG_LASTCOMMAND;
+	if (flags & async_mask)
+		dsi_flags |= EXYNOS_DSI_MSG_QUEUE;
 
 	c = &cmd_set->cmds[cmd_set->num_cmd - 1];
 	if (!c->panel_rev) {
@@ -710,7 +710,7 @@ void exynos_panel_send_cmd_set_flags(struct exynos_panel *ctx,
 			continue;
 
 		if ((c == last_cmd) && !(flags & PANEL_CMD_SET_QUEUE))
-			dsi_flags |= MIPI_DSI_MSG_LASTCOMMAND;
+			dsi_flags &= ~EXYNOS_DSI_MSG_QUEUE;
 
 		exynos_dsi_dcs_write_buffer(dsi, c->cmd, c->cmd_len, dsi_flags);
 		if (delay_ms)
@@ -1908,7 +1908,6 @@ static int exynos_dsi_debugfs_add(struct mipi_dsi_device *dsi,
 		return -ENOMEM;
 
 	reg_data->dsi = dsi;
-	reg_data->flags = MIPI_DSI_MSG_LASTCOMMAND;
 
 	debugfs_create_u8("address", 0600, reg_root, &reg_data->address);
 	debugfs_create_u8("type", 0600, reg_root, &reg_data->type);

commit d0c3458df77f84994dce93a471fa291fd9256929

samsung: update support for EXYNOS_DSI_MSG_QUEUE
Commit 14290d53b8c7 ("samsung: add flags to dsim to force mipi commands
batching") took bit 13 which interferes with EXYNOS_DSI_MSG_QUEUE.
Update EXYNOS_DSI_MSG_QUEUE to use bit 15 instead. Since the production
driver is decrementing the count for each new flag, we should be okay
now to take merges without breaking this feature.

We also need to update any new references to MIPI_DSI_MSG_LASTCOMMAND to
use EXYNOS_DSI_MSG_QUEUE instead.

Fixes: 64d97619a2b0 ("GKI: drm: samsung: introduce EXYNOS_DSI_MSG_QUEUE flag")
Signed-off-by: Will McVicker <willmcvicker@google.com>
Change-Id: I0d195e1152f81366027f05968a8d97fcb04984f1

samsung/exynos_drm_connector.h
@@ -38,7 +38,7 @@ struct exynos_drm_connector;
 
 /* Stack all commands until lastcommand bit and trigger all in one go */
 #ifndef EXYNOS_DSI_MSG_QUEUE
-#define EXYNOS_DSI_MSG_QUEUE BIT(13)
+#define EXYNOS_DSI_MSG_QUEUE BIT(15)
 #endif
 
 /* packetgo feature to batch msgs can wait for vblank, use this flag to ignore */

samsung/exynos_drm_dsim.h
@@ -113,7 +113,7 @@ struct dsim_device {
 	int idle_ip_index;
 	u8 total_pend_ph;
 	u16 total_pend_pl;
-	/* override message flag MIPI_DSI_MSG_LASTCOMMAND */
+	/* override message flag ~EXYNOS_DSI_MSG_QUEUE */
 	bool force_batching;
 
 	enum dsim_dual_dsi dual_dsi;

samsung/panel/panel-samsung-drv.h
@@ -797,18 +797,16 @@ static inline void te2_state_changed(struct backlight_device *bl)
 } while (0)
 
 #define EXYNOS_DCS_BUF_ADD(ctx, seq...) \
-	EXYNOS_DCS_WRITE_SEQ_FLAGS(ctx, 0, seq)
+	EXYNOS_DCS_WRITE_SEQ_FLAGS(ctx, EXYNOS_DSI_MSG_QUEUE, seq)
 
 #define EXYNOS_DCS_BUF_ADD_SET(ctx, set) \
-	EXYNOS_DCS_WRITE_TABLE_FLAGS(ctx, set, 0)
+	EXYNOS_DCS_WRITE_TABLE_FLAGS(ctx, set, EXYNOS_DSI_MSG_QUEUE)
 
 #define EXYNOS_DCS_BUF_ADD_AND_FLUSH(ctx, seq...) \
-	EXYNOS_DCS_WRITE_SEQ_FLAGS(ctx, EXYNOS_DSI_MSG_IGNORE_VBLANK | \
-		MIPI_DSI_MSG_LASTCOMMAND, seq)
+	EXYNOS_DCS_WRITE_SEQ_FLAGS(ctx, EXYNOS_DSI_MSG_IGNORE_VBLANK, seq)
 
 #define EXYNOS_DCS_BUF_ADD_SET_AND_FLUSH(ctx, set) \
-	EXYNOS_DCS_WRITE_TABLE_FLAGS(ctx, set, EXYNOS_DSI_MSG_IGNORE_VBLANK | \
-		MIPI_DSI_MSG_LASTCOMMAND)
+	EXYNOS_DCS_WRITE_TABLE_FLAGS(ctx, set, EXYNOS_DSI_MSG_IGNORE_VBLANK)
 
 #define DSC_PPS_SIZE sizeof(struct drm_dsc_picture_parameter_set)
 