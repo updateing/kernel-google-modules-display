2d8bc195267c29ba9629efccb5bba3df70c856ff
Yuti Amonkar <yuti.amonkar@samsung.com>
2022-11-23 05:20:28 +0000

    drm: samsung: Add support to configure AFBC BLK_SIZE
    Change-Id: Ie7fad8c4253eecb8955e279438cc5d4df0f231bc
    Signed-off-by: Yuti Amonkar <yuti.amonkar@samsung.com>

--- samsung/cal_9865/dpp_reg.c
+++ samsung/cal_9865/dpp_reg.c
@@ -1209,6 +1209,11 @@ void dpp_reg_configure_params(u32 id, struct dpp_params_info *p,
 	if (test_bit(DPP_ATTR_AFBC, &attr) || test_bit(DPP_ATTR_SBWC, &attr))
 		idma_reg_set_comp(id, p->comp_type, p->rcv_num);
 
+	if (test_bit(DPP_ATTR_AFBC, &attr))
+		dma_write_mask(id, RDMA_AFBC_PARAM,
+				IDMA_AFBC_BLK_SIZE(p->blk_size - 1),
+				IDMA_AFBC_BLK_SIZE_MASK);
+
 	if (test_bit(DPP_ATTR_SBWC, &attr))
 		dma_write_mask(id, RDMA_SBWC_PARAM,
 				IDMA_CHM_BLK_BYTENUM(p->blk_size) |
