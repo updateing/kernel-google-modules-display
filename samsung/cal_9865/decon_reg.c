2f8ffca50f03a5f5421687f43e846e6a0aa57c3d
Leo Chen <yinchiuan@google.com>
2023-04-07 06:21:17 +0000

    drm: samsung: cal_9865: correct the decon data path config
    COMP_DSC(0) means
        1. DSC_ENC0 in DSC_TOP0 for DECON 0
        2. DSC_ENC1 in DSC_TOP0 for DECON 1
        3. DSC_ENC4 in DSC_TOP2 for DECON 2
    COMP_DSC(1) means
        1. DSC_ENC1 in DSC_TOP0 for DECON 0
        2. DSC_ENC5 in DSC_TOP2 for DECON 2
    
    Test: Dual display work in OS
    Bug: 271798151
    Change-Id: Ibcc1973324ebcef2ead346925770e1064fcd5e62
    Signed-off-by: Leo Chen <yinchiuan@google.com>

--- samsung/cal_9865/decon_reg.c
+++ samsung/cal_9865/decon_reg.c
@@ -32,6 +32,15 @@ enum decon_dsc_id {
 	DECON_DSC_ENC2 = 0x2,
 };
 
+enum decon_fifo {
+	DECON0_OFIFO0 = 0x0,
+	DECON0_OFIFO1 = 0x1,
+	DECON1_OFIFO0 = 0x2,
+	DECON_OFIFO_RSVD = 0x3,
+	DECON2_OFIFO0 = 0x4,
+	DECON_OFIFO_NONE = 0xF,
+};
+
 enum decon_win_alpha_coef {
 	BND_COEF_ZERO		  = 0x0,
 	BND_COEF_ONE		  = 0x1,
@@ -337,17 +346,40 @@ static void decon_reg_set_dta_threshold(u32 id, u32 high, u32 low)
 #define OUTIF_DSI1	BIT(1)
 #define OUTIF_WB	BIT(2)
 #define OUTIF_DPIF	BIT(3)
-#define COMP_DSC(id)	BIT(id + 4) /* DSC 0,1,2 */
+/*
+ * COMP_DSC(0) means
+ *    1. DSC_ENC0 in DSC_TOP0 for DECON 0
+ *    2. DSC_ENC1 in DSC_TOP0 for DECON 1
+ *    3. DSC_ENC4 in DSC_TOP2 for DECON 2
+ * COMP_DSC(1) means
+ *    1. DSC_ENC1 in DSC_TOP0 for DECON 0
+ *    2. DSC_ENC5 in DSC_TOP2 for DECON 2
+ */
+#define COMP_DSC(id)	BIT((id) + 4) /* COMP_DSC 0,1 */
 #define COMP_DSCC	BIT(7)
+
+static void decon_reg_log_dsimif(u32 id, u32 dsimif, enum decon_fifo sel)
+{
+	if (sel <= DECON2_OFIFO0 && sel != DECON_OFIFO_RSVD)
+		cal_log_debug(id, "sel(0x%X) OFIFO.%u - dsim%u\n", sel, sel%2, dsimif);
+	else
+		cal_log_debug(id, "sel(0x%X) not connect to dsim%u\n", sel, dsimif);
+}
+
+static void decon_reg_dsimif_write(u32 id, u32 dsimif, enum decon_fifo sel)
+{
+	decon_reg_log_dsimif(id, dsimif, sel);
+	dsimif_write(id, DSIMIF_SEL(dsimif), SEL_DSIM(sel));
+}
 
 static void decon_reg_clear_dsimif(u32 id, u32 dsimif)
 {
 	u32 val;
 
 	val = SEL_DSIM_GET(dsimif_read(id, DSIMIF_SEL(dsimif)));
-	if ((id == 0 && val < 2) || (id == 1 && val == 2)) {
+	if ((id == 0 && val < DECON1_OFIFO0) || (id == 1 && val == DECON1_OFIFO0)) {
 		cal_log_info(id, "clearing dsimif%u sel, val: %u\n", dsimif, val);
-		dsimif_write(id, DSIMIF_SEL(dsimif), SEL_DSIM(3));
+		decon_reg_dsimif_write(id, dsimif, DECON_OFIFO_NONE);
 	}
 }
 
@@ -357,11 +389,16 @@ static void decon_reg_set_data_path(u32 id, struct decon_config *cfg)
 	u32 dsc_count = cfg->dsc.dsc_count;
 	u32 val;
 
+	if (id > 2) {
+		cal_log_err(id, "invalid decon\n");
+		return;
+	}
+
 	switch (out_type) {
 	case DECON_OUT_DSI0:
 		val = OUTIF_DSI0;
 		decon_reg_clear_dsimif(id, 1);
-		dsimif_write(id, DSIMIF_SEL(0), SEL_DSIM(id == 1 ? 2 : 0));
+		decon_reg_dsimif_write(id, 0, id == 1 ? DECON1_OFIFO0 : DECON0_OFIFO0);
 		break;
 	/*
 	 * OUTIF_DSIx in DECON determines that blended data is transferred
@@ -373,12 +410,12 @@ static void decon_reg_set_data_path(u32 id, struct decon_config *cfg)
 	case DECON_OUT_DSI1:
 		val = OUTIF_DSI0;
 		decon_reg_clear_dsimif(id, 0);
-		dsimif_write(id, DSIMIF_SEL(1), SEL_DSIM(id == 1 ? 2 : 0));
+		decon_reg_dsimif_write(id, 1, id == 1 ? DECON1_OFIFO0 : DECON0_OFIFO0);
 		break;
 	case DECON_OUT_DSI:
 		val = OUTIF_DSI0 | OUTIF_DSI1;
-		dsimif_write(id, DSIMIF_SEL(0), SEL_DSIM(0));
-		dsimif_write(id, DSIMIF_SEL(1), SEL_DSIM(1));
+		decon_reg_dsimif_write(id, 0, DECON0_OFIFO0);
+		decon_reg_dsimif_write(id, 1, DECON0_OFIFO1);
 		break;
 	case DECON_OUT_DP0:
 		val = OUTIF_DPIF;
@@ -399,19 +436,16 @@ static void decon_reg_set_data_path(u32 id, struct decon_config *cfg)
 	}
 
 	if (dsc_count == 2) {
-		if (id != 0) {
+		if (id == 1) {
 			cal_log_err(id, "unsupported dsc path\n");
 			return;
 		}
 		val |= COMP_DSCC | COMP_DSC(1) | COMP_DSC(0);
 	} else if (dsc_count == 1) {
-		if (id > 2) {
-			cal_log_err(id, "invalid decon id(%d)\n", id);
-			return;
-		}
-		val |= COMP_DSC(id);
+		val |= COMP_DSC(0);
 	}
 
+	cal_log_debug(id, "d_path(0x%02X)", val);
 	decon_write_mask(id, DATA_PATH_CON_0, COMP_OUTIF_PATH_F(val),
 			COMP_OUTIF_PATH_MASK);
 }
